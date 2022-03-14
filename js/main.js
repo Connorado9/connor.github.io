522df7be201164b12b37ae21edf441814352385e
Connor Humiston <chumiston2@gmail.com>
2022-03-13 19:19:51 -0600

    Restarting Project

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/* Toggle the sidebar */
+/*
+function toggleSidebar(){
+  document.getElementById("sidebar").classList.toggle('active');
+}
+*/
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+/*function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}*/
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+/*function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}*/
