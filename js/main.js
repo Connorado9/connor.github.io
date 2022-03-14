015c57913f21d15a06b0a291b67f494a50648b72
Connor Humiston <chumiston2@gmail.com>
2022-03-14 02:27:17 -0600

    Min height for mobile cover update

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ function toggleSidebar(){
   document.getElementById("main").style.marginLeft = "0";
 }*/
 
+/*
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
@@ -28,3 +29,4 @@ window.addEventListener('resize', () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
+*/
