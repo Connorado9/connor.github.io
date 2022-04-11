dd68d02e49e93dc748c09e0223e5a31f63120d15
Connor Humiston <chumiston2@gmail.com>
2022-04-10 18:57:19 -0600

    update api

--- pages/assignments/week12-demo.js
+++ pages/assignments/week12-demo.js
@@ -22,19 +22,20 @@ const endpoint = 'https://catfact.ninja/fact';
 const dogpoint = 'https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1';
 const dogpic = 'https://dog.ceo/dog-api/';
 const dogfacts = 'https://dog-api.kinduff.com/api/facts?number=1'
+const dogfact = 'https://dogfacts-api.herokuapp.com/api/v1/resources/dogs?number=1'
 
 /* now we need to do something */
 async function getQuote() {
   console.log("quote button was clicked"); /* debug */
   try { /* tries something to see if there's an error */
-    const response = await fetch(dogfacts);
+    const response = await fetch(dogfact);
 
     const json = await response.json(); // javascript object notiation (json is file format that is human and machine readable)
     if(!response.ok) {
       throw Error(response.statusText);
     }
     console.log(json);
-    displayQuote(json.facts[0]); //json[0] for array
+    displayQuote(json[0].facts); //json[0] for array
   } catch(err) { /* if there isnt, do something else instead */
     console.log(err);
     alert('Failed');
