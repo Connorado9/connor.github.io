e36aa9027810607a756fb9648d7f9c3cbc1658f5
Connor Humiston <chumiston2@gmail.com>
2022-04-10 18:32:55 -0600

    Update facts api

--- pages/assignments/week12-demo.js
+++ pages/assignments/week12-demo.js
@@ -21,23 +21,24 @@ quoteButton.addEventListener('click', getQuote);
 const endpoint = 'https://catfact.ninja/fact';
 const dogpoint = 'https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1';
 const dogpic = 'https://dog.ceo/dog-api/';
+const dogfacts = 'https://dog-api.kinduff.com/api/facts?number=1'
 
 /* now we need to do something */
 async function getQuote() {
   console.log("quote button was clicked"); /* debug */
-  //try { /* tries something to see if there's an error */
-    const response = await fetch(dogpoint);
+  try { /* tries something to see if there's an error */
+    const response = await fetch(dogfacts);
 
     const json = await response.json(); // javascript object notiation (json is file format that is human and machine readable)
     if(!response.ok) {
       throw Error(response.statusText);
     }
     console.log(json);
-    displayQuote(json[0].fact); //json[0] for array
-  //} catch(err) { /* if there isnt, do something else instead */
-  //  console.log(err);
-  //  alert('Failed');
-  //}
+    displayQuote(json.facts[0]); //json[0] for array
+  } catch(err) { /* if there isnt, do something else instead */
+    console.log(err);
+    alert('Failed');
+  }
 }
 
 function displayQuote(quote) {
