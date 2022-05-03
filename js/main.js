e55598d62e548b6d5ea208835dbc2a5a55cf4b54
Connor Humiston <chumiston2@gmail.com>
2022-05-03 15:07:04 -0600

    Update mobile view, upload images for prj1

--- js/main.js
+++ js/main.js
@@ -1,21 +1,41 @@
-/* Sidebar Toggles */
+/*** Sidebar Toggles ***/
 /* Select the ID 'main', and listen for a click */
 document.getElementById('burger').addEventListener('click', openNav);
 document.getElementById('overlay').addEventListener('click', closeNav);
-
 /* Function for expanding the sidebar */
-function openNav() {
+function openNav()
+{
   document.getElementById("sidenavigation").style.width = "250px";
   document.getElementById("overlay").style.display = "block";
   console.log('hamburger clicked open, overlay on'); /* for debugging, see console on browser */
 }
-
 /* Function for minimizing the sidebar */
-function closeNav() {
+function closeNav()
+{
   document.getElementById("sidenavigation").style.width = "0";
   document.getElementById("overlay").style.display = "none";
   console.log('hamburger clicked closed, overlay off'); /* for debugging, see console on browser */
 }
+
+/*** Resume Expereince vs Education ***/
+document.getElementById('edu-btn').addEventListener('click', showEducation)
+document.getElementById('exp-btn').addEventListener('click', showExperience);
+function showExperience()
+{
+  document.getElementById('exp-btn').style.color = "#1B1B1B";
+  document.getElementById('edu-btn').style.color = "#969595";
+  document.getElementById('experience').style.display = "grid";
+  document.getElementById('education').style.display = "none";
+}
+function showEducation()
+{
+  document.getElementById('exp-btn').style.color = "#969595";
+  document.getElementById('edu-btn').style.color = "#1B1B1B";
+  document.getElementById('experience').style.display = "none";
+  document.getElementById('education').style.display = "grid";
+}
+window.onload = showExperience;
+
 
 /* Scrolling Event Listener */
 function windowLoaded () {
