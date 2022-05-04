70284c8be2c981b775562eb20ab65faf8592fb15
Connor Humiston <chumiston2@gmail.com>
2022-05-04 01:16:23 -0600

    animate project 1

--- js/gridmon.js
+++ js/gridmon.js
@@ -16,3 +16,23 @@ function closeNav()
   document.getElementById("overlay").style.display = "none";
   console.log('hamburger clicked closed, overlay off'); /* for debugging, see console on browser */
 }
+
+
+/*** Reveal Effect ***/
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal"); //select reveal elements
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight; //height of viewport
+    var elementTop = reveals[i].getBoundingClientRect().top; //distance from top of viewport
+    var elementVisible = 150; //height at which the element should be revealed
+
+    if (elementTop < windowHeight - elementVisible) { //trigger animation
+      reveals[i].classList.add("active"); //display the element
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
